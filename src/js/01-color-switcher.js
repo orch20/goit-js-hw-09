794846de39510a004a86f3fd2002ddcc878ed3f7
orch20 <94862153+orch20@users.noreply.github.com>
2022-06-01 00:24:52 +0300

    color switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+};
 
+refs.buttonStart.addEventListener('click', startChangingColor);
+refs.buttonStop.addEventListener('click', stopChangingColor);
+refs.buttonStop.toggleAttribute('disabled');
+
+let timerId = null;
+const INTERVAL = 1000;
+
+function setColor() {
+  color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
+
+function startChangingColor() {
+  timerId = setInterval(setColor, INTERVAL);
+  refs.buttonStart.toggleAttribute('disabled');
+  refs.buttonStop.toggleAttribute('disabled');
+}
+
+function stopChangingColor() {
+  clearInterval(timerId);
+  refs.buttonStop.toggleAttribute('disabled');
+  refs.buttonStart.toggleAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
