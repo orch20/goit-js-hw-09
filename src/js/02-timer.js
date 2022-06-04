b4ba374682b12dee43da877f145d252fd11a4cb2
orch20 <94862153+orch20@users.noreply.github.com>
2022-06-04 20:38:40 +0300

    set date

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   button: document.querySelector('[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
-// let userData;
-
 let currentTime = new Date();
-console.log('currentTime', currentTime);
+let userData = null;
 
 refs.button.toggleAttribute('disabled');
 
@@ -19,43 +23,67 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-    // userData = +selectedDates;
-    dataCompare(Number(...selectedDates));
+    userData = Number(...selectedDates);
+    dataCompare();
   },
 };
 
 flatpickr('#datetime-picker', options);
 
-function dataCompare(selectedDatesByUser) {
-  console.log('selectedDatesByUser', selectedDatesByUser);
-  if (selectedDatesByUser < currentTime) {
-    alert('Please choose a date in the future');
+function dataCompare() {
+  console.log('selectedDatesByUser', userData);
+  if (userData < currentTime) {
+    // alert('Please choose a date in the future');
+    Notify.failure('Please choose a date in the future');
   }
-  startCountdown(selectedDatesByUser);
-}
-
-function startCountdown(selectedDatesByUser) {
   refs.button.toggleAttribute('disabled');
-  console.log(selectedDatesByUser - currentTime);
-  startTimer(selectedDatesByUser - currentTime);
 }
 
-function startTimer(duration, display) {
-  var timer = duration,
-    minutes,
-    seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
+refs.button.addEventListener('click', startCountdown);
+// let countdown;
 
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+console.log();
 
-    // display.textContent = minutes + ':' + seconds;
-    console.log(minutes + ':' + seconds);
-
-    if (--timer < 0) {
-      timer = duration;
+function startCountdown() {
+  const timerId = setInterval(() => {
+    let delta = userData - Date.now();
+    console.log(delta);
+    const countdown = convertMs(delta);
+    upgradeRes(countdown);
+    if (delta === 0) {
+      clearInterval(timerId);
+      return;
     }
   }, 1000);
+}
+
+function upgradeRes({ days, hours, minutes, seconds }) {
+  refs.seconds.textContent = seconds;
+  refs.minutes.textContent = minutes;
+  refs.hours.textContent = hours;
+  refs.days.textContent = days;
+  console.log(`${minutes},${seconds}`);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addingZero(value) {
+  return String(value).padStart(2, '0');
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
+
+ const delay =  document.querySelector('[name="delay"]'),
+ const step =  document.querySelector('[name="step"]'),
+  const amount =  document.querySelector('[name="amount"]'),
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  
+  new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve) {
+    resolve
   } else {
-    // Reject
+    reject
+  }, delay);
+});
   }
 }
+
