d7be02c92eaa45fd6eef21a68b551b15aafa8b9d
orch20 <94862153+orch20@users.noreply.github.com>
2022-06-05 14:06:23 +0300

    third task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,36 @@
+const form = document.querySelector('form');
+const { delay, step, amount } = form;
 
- const delay =  document.querySelector('[name="delay"]'),
- const step =  document.querySelector('[name="step"]'),
-  const amount =  document.querySelector('[name="amount"]'),
-
+form.addEventListener('submit', onSubmit);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  
-  new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (shouldResolve) {
-    resolve
-  } else {
-    reject
-  }, delay);
-});
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
+function onSubmit(e) {
+  e.preventDefault();
+  if (!+delay.value || !+step.value || !+amount.value) {
+    alert('hha-ha');
+  }
+
+  for (i = 0; i <= amount.value; i += 1) {
+    delayValue = Number(delay.value + step.value * i);
+    createPromise(1 + i, delayValue)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
